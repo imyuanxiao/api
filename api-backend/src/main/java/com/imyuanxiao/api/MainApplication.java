08f4f38de688baea9d216115bc3b8b87275e9f7d
imyuanxiao <imyuanxiao@126.com>
2023-06-07 17:44:28 +0100

    init

---
+++ api-backend/src/main/java/com/imyuanxiao/api/MainApplication.java
@@ -0,0 +1,26 @@
+package com.imyuanxiao.api;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 主类（项目启动入口）
+ *
+ * @template <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @author <a href="https://github.com/imyuanxiao">imyuanxiao</a>
+ *
+ */
+@SpringBootApplication
+@MapperScan("com.imyuanxiao.api.mapper")
+@EnableScheduling
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
